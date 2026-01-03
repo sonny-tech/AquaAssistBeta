815a45cb33fb5e46764b7f3e1ae4ae2d86790c05
Sonny <sonnyraminafshar@gmail.com>
2026-01-02 16:25:22 -0800

    removed filter section function to prevent elimination of drills

--- app.py
+++ app.py
@@ -121,18 +121,18 @@ def display_pdf(path):
         unsafe_allow_html=True
     )
 
-#-----------------HELPER FUNCTION TO FILTER LINES-----------------
-def filter_section_content(content, requested_strokes):
-    if not requested_strokes:
-        return content
+#-----------------HELPER FUNCTION TO FILTER LINES (removed) -----------------
+#def filter_section_content(content, requested_strokes):
+   # if not requested_strokes:
+   #     return content
 
-    filtered_lines = []
-    for line in content.split("\n"):
-        if any(stroke in line.lower() for stroke in requested_strokes):
-            filtered_lines.append(line)
+    #filtered_lines = []
+    #for line in content.split("\n"):
+    #    if any(stroke in line.lower() for stroke in requested_strokes):
+    #        filtered_lines.append(line)
 
-    return "\n".join(filtered_lines)
-
+   # return "\n".join(filtered_lines)
+#----------------------------------------------------------------------------
 # ------------------ UI ------------------
 
 st.caption("Example: **'Swimmer 6 front crawl'** or **'Junior Masters 1 butterfly'**")
@@ -166,15 +166,10 @@ if user_input:
 
                 if content:
                     st.write(f"### {header}")
-                    filtered = filter_section_content(content, requested_strokes)
-
-                    if filtered.strip():
-                        st.text(filtered)
-                    else:
-                        st.text(content)  # fallback if filtering removes everything
-
+                    st.text(content)
                 else:
-                     st.write("That section was not found in this lesson plan.")
+                    st.write("That section was not found in this lesson plan.")
+
 
             else:
                 st.write(f"### {matched_level.title()} — Full Lesson Plan")
